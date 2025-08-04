514e6866ddc65d0da3cdf3c7621244f38ef5a70f
Azola-Q <159591312+Azola-Q@users.noreply.github.com>
2025-08-04 19:41:22 +0200

    Update backends.py

--- core/core/backends.py
+++ core/core/backends.py
@@ -1,1 +1,21 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class IDNumberBackend(ModelBackend):
+    """
+    Custom authentication backend that allows users to log in using their
+    ID number (stored in the 'id_number' field) instead of username.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(id_number=username)
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
 
