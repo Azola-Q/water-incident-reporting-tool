e75d8604e0806a848f10361cf79ce3491a5982ab
Azola-Q <159591312+Azola-Q@users.noreply.github.com>
2025-08-05 13:44:20 +0200

    Update one_time_superuser.py

--- one_time_superuser.py
+++ one_time_superuser.py
@@ -8,9 +8,19 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "adminpassword123")
+# Use a unique ID number instead of "username"
+if not User.objects.filter(id_number="0000000000000").exists():
+    User.objects.create_superuser(
+        id_number="0000000000000",
+        email="admin@example.com",
+        password="adminpassword123",
+        first_name="Admin",
+        last_name="User",
+        phone_number="0123456789",
+        is_staff=True,
+        is_superuser=True,
+        is_admin=True
+    )
     print("✅ Superuser created")
 else:
     print("ℹ️ Superuser already exists")
-
